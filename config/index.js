f770b2f8729dc663057448f6a88e042aa0bd9878
David Cramer <dcramer@gmail.com>
2018-09-08 13:37:13 -0700

    Switch to public heroku endpoint

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
 export const server = {
-  apiURL: __DEV__ ? 'http://localhost:8000/graphql/' : 'http://example.com/graphql/',
+  apiURL: __DEV__
+    ? 'http://localhost:8000/graphql/'
+    : 'https://calm-thicket-87592.herokuapp.com/graphql/',
 };
